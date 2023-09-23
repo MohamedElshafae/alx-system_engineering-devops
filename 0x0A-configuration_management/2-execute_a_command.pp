ca13ba55de11fa8352bc7f2f5ce68818a4260cfb
Elshafae <maohmedelshafae888@gmail.com>
2023-09-23 03:22:17 +0300

    try task2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,6 @@
 #a manifest that kills a process named
 
 exec {'pkill killmenow':
-    path     => '/usr/bin',
     command  => 'pkill killmenow',
     provider => shell,
-    returns  => [0, 1], 
 }
