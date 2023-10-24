ed81d7e1889c23de34aea405d1d4bd7f9b7e8ef4
Elshafae <maohmedelshafae888@gmail.com>
2023-10-25 02:28:45 +0300

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     resource = get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
     tasks = get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
-    file_path = '{argv[1]}.csv'
+    file_path = f'{argv[1]}.csv'
 
     resource = resource.json()
     tasks = tasks.json()
