80ded32a68da3cfd8239b721cc52bda5d2591935
Elshafae <maohmedelshafae888@gmail.com>
2023-10-25 02:21:16 +0300

    1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Python script to export data in the CSV format"""
+
+from requests import get
+from sys import argv
+
+
+if __name__ == "__main__":
+
+    resource = get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+    tasks = get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}/todos')
+    file_path = 'USER_ID.csv'
+
+    resource = resource.json()
+    tasks = tasks.json()
+    formated_str = ''
+    for task in tasks:
+        formated_str += f'"{argv[1]}","{resource["name"]}",'
+        formated_str += f'"{task["completed"]}","{task["title"]}"\n'
+    with open(file_path, "w") as file:
+        file.write(formated_str)
