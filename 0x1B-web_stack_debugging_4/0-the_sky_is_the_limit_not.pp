90bc2ad8d36c4f598037888413aada1eb423129b
root <root@Eshafae>
2023-11-19 03:59:53 +0200

    tring

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,10 @@
 # increase the limt of Nginx
 
-
-package { 'nginx':
-  ensure => installed,
-}
-
-service { 'nginx':
-  ensure  => running,
-  enable  => true,
-  require => Package['nginx'],
-}
-
 exec { 'increase-limt':
-  command => '/bin/sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/',
+  provider    => 'shell',
+  command     => 'sudo sed -i "s/15/4096/" /etc/default/nginx',
 }
 exec { 'nginx-restart':
-  command     => 'etc/init.d/nginx restart',
-  path        => 'etc/init.d/'
+  provider    => 'shell',
+  command     => 'sudo service nginx restart',
 }
